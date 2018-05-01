297c3c28a90fa6620dc97630d710d9809c73c7a7
dh <dhfromkorea@gmail.com>
2018-05-01 15:35:21 -0400

    modify experiment id

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 # Experiment 3
 
 # SGD 
-python3 main_torch.py --batch_size 1 --init_weight_type "simple" --exp_id "exp1_sgd" --grad_clip --grad_clip_value 10.0 --lr 0.0001 --simple_init_std 1.0 --weight_decay 0.01
+python3 main_torch.py --batch_size 1 --init_weight_type "simple" --exp_id "exp3_sgd" --grad_clip --grad_clip_value 10.0 --lr 0.0001 --simple_init_std 1.0 --weight_decay 0.01
 
 # SGD w/ noise
-python3 main_torch.py --batch_size 1 --grad_noise --eta 0.01 --gamma 0.55 --init_weight_type "simple" --exp_id "exp1_sgd_noise" --grad_clip --grad_clip_value 10.0 --lr 0.0001 --simple_init_std 1.0 --weight_decay 0.01
+python3 main_torch.py --batch_size 1 --grad_noise --eta 0.01 --gamma 0.55 --init_weight_type "simple" --exp_id "exp3_sgd_noise" --grad_clip --grad_clip_value 10.0 --lr 0.0001 --simple_init_std 1.0 --weight_decay 0.01
 
 
 # experiment 5
